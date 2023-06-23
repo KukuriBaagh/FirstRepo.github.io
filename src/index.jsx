10515a62988b6306b13a1ddf3fe32fd6f429f4ac
KukuriBaagh <howtoccdemo@gmail.com>
2023-06-23 12:07:52 +0530

    fixed index. jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-
-React.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
