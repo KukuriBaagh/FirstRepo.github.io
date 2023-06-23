b0f5cb9ef914b4b771d7f9bc74754b88d539e9fe
KukuriBaagh <howtoccdemo@gmail.com>
2023-06-23 12:56:21 +0530

    navbar phase 2

--- src/compoenents/static/Navbar.jsx
+++ src/compoenents/static/Navbar.jsx
@@ -5,13 +5,14 @@ import ReactDOM from 'react-dom/client'
 function Navbar(){
     return (
         <>
-        <nav>
+        <nav className='mt-2 rounded-sm'>
             <ul className='flex flex-row rounded shadow-md h-10 text-xl'>
-                <li className='hover:bg-slate-200 m-auto'>Hi There</li>
-                <li className='m-auto'>Home</li>
-                <li className='m-auto'>Profile</li>
-                <li className='m-auto'>Contact</li>
-                <li className='m-auto'>Workspace</li>
+                <li className='p-1 rounded-full hover:bg-slate-200 m-auto'><a href="#">Hi There</a></li>
+                <li className='p-1 rounded-full hover:bg-slate-200 m-auto'><a href="#home">Home</a></li>
+                <li className='p-1 rounded-full hover:bg-slate-200 m-auto'><a href="#profile">Profile</a></li>
+                <li className='p-1 rounded-full hover:bg-slate-200 m-auto'><a href="#contact">Contact</a></li>
+                <li className='p-1 rounded-full hover:bg-slate-200 m-auto'><a href="#workspace">My Workspace</a></li>
+                <li><input className=' mr-0 p-1 rounded-full hover:bg-slate-200 m-auto' type="text" placeholder="Search Here..." /></li>
             </ul>
         </nav>
         </>
