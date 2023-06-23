a2df6acc5ddaf59d1074f5825a55226c266eeb8b
KukuriBaagh <howtoccdemo@gmail.com>
2023-06-23 12:20:46 +0530

    navbar phase1

--- src/compoenents/static/Navbar.jsx
+++ src/compoenents/static/Navbar.jsx
@@ -1,14 +1,17 @@
 import './Navbar.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 function Navbar(){
     return (
         <>
         <nav>
-            <ul>
-                <li>Home</li>
-                <li>Profile</li>
-                <li>Contact</li>
-                <li>Workspace</li>
+            <ul className='flex flex-row rounded shadow-md h-10 text-xl'>
+                <li className='hover:bg-slate-200 m-auto'>Hi There</li>
+                <li className='m-auto'>Home</li>
+                <li className='m-auto'>Profile</li>
+                <li className='m-auto'>Contact</li>
+                <li className='m-auto'>Workspace</li>
             </ul>
         </nav>
         </>
