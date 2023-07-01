a4b0d589c8cf303a151204308ce8993e68e2f8ef
KukuriBaagh <howtoccdemo@gmail.com>
2023-07-01 13:55:09 +0530

    completed login form with yup validation and formik chakra ui controlled form

--- src/compoenents/UXs/LoginForm.jsx
+++ src/compoenents/UXs/LoginForm.jsx
@@ -1,10 +1,10 @@
 import React from "react"
 import { Input } from "@chakra-ui/input"
 import { Heading, VStack } from "@chakra-ui/layout"
-import { FormControl, FormErrorMessage, FormLabel } from "@chakra-ui/form-control"
 import { Button } from "@chakra-ui/button"
 import { Field, Formik, useFormik } from "formik"
 import * as Yup from "yup"
+import TextField from "./TextField"
 
 const LoginForm = () => {
 
@@ -15,6 +15,7 @@ const LoginForm = () => {
         validationSchema={Yup.object({
           username: Yup.string().required("Username required").min(2, "Username atleast 2 Characters"),
           password: Yup.string().required("Password is Required").min(6, "Password is too Short"),
+          email: Yup.string().email("invalid email").required("required email")
         })}
         onSubmit={(values, actions) => {
           alert(JSON.stringify(values, null, 2))
@@ -31,34 +32,14 @@ const LoginForm = () => {
           onSubmit={formik.handleSubmit}
           >
           <Heading>Sign Up</Heading>
-  
-          <FormControl isInvalid={formik.errors.username && formik.touched.username}>
-              <FormLabel>Username</FormLabel>
-              <Field
-              as={Input}
-               name="username"
-              placeholder="enter username..."
-              />
-              <FormErrorMessage>{formik.errors.username}</FormErrorMessage>
-          </FormControl>
-  
-          <FormControl isInvalid={formik.errors.password && formik.touched.password}>
-              <FormLabel>Password</FormLabel>
-              <Field
-              as={Input}
-              name="password"
-              type="password"
-              placeholder="enter password..."
-              />
-              <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
-          </FormControl>
-  
+          <TextField name="username" type="username" placeholder="enter username" />
+          <TextField name="email" type="email" placeholder="enter email" />
+          <TextField name="password" type="password" placeholder="enter password" />
           <Button type="submit" variant="outline" colorScheme="teal">
             Create Account
           </Button>
       </VStack>
         )}
-
       </Formik>
     </>
   )
